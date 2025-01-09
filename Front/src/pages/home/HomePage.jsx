8b7c2ca831a6b64d0b59950f105323288568a8fc
OFEKve <ofekve1003@gmail.com>
2025-01-09 14:19:41 +0200

    Fix case sensitivity for AuthScreen.jsx

--- Front/src/pages/home/HomePage.jsx
+++ Front/src/pages/home/HomePage.jsx
@@ -1,9 +1,9 @@
 import React from "react"
 import HomeScreen from "./HomeScreen"
-import Authscreen from "./Authscreen"
+import AuthScreen from "./AuthScreen"
 import { useAuthStore } from "../../store/authUser"
 const HomePage = () => {
   const { user } = useAuthStore()
-  return <div>{user ? <HomeScreen /> : <Authscreen />}</div>
+  return <div>{user ? <HomeScreen /> : <AuthScreen />}</div>
 }
 export default HomePage
