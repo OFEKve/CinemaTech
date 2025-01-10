321a23b48f7cc03da2df7925ce1c5cc07a52020e
OFEKve <ofekve1003@gmail.com>
2025-01-10 13:24:00 +0200

    Update CORS and APi

--- Backend/server.js
+++ Backend/server.js
@@ -8,14 +8,27 @@ import routes from "./route.js";
 const app = express();
 
 app.use(express.json());
-app.use(cookieParser());
+// הוסף את זה לפני הגדרות ה-CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
+
 app.use(
   cors({
     origin: ["http://localhost:5173", "https://ofekvegastech.netlify.app"],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    exposedHeaders: ["Set-Cookie"],
   })
 );
 
+app.use(cookieParser());
 const PORT = ENV_VARS.PORT;
 
 app.use("/api/v1", routes);
--- Backend/utils/generateToken.js
+++ Backend/utils/generateToken.js
@@ -2,13 +2,16 @@ import jwt from "jsonwebtoken";
 import { ENV_VARS } from "../config/envVars.js";
 
 export const generateTokenAndSetCookie = (userId, res) => {
-  const token = jwt.sign({ userId }, ENV_VARS.JWT_SECRET, { expiresIn: "15d" });
+  const token = jwt.sign({ userId }, ENV_VARS.JWT_SECRET, {
+    expiresIn: "30d",
+  });
 
   res.cookie("jwt-netflix", token, {
-    maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days in MS
-    httpOnly: true, // prevent XSS attacks cross-site scripting attacks, make it not be accessed by JS
-    sameSite: "strict", // CSRF attacks cross-site request forgery attacks
-    secure: ENV_VARS.NODE_ENV !== "development",
+    httpOnly: true,
+    secure: true, // true בגלל HTTPS
+    sameSite: "none", // חשוב ל-cross-origin
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+    path: "/",
   });
 
   return token;
